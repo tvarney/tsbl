73f45412d3f7c0868d05d6e3423625c1da127b7d
tvarney <troy.a.varney@gmail.com>
2018-09-04 12:17:01 -0500

    Made the repl print out unicode categories for its arguments

--- source/repl.cpp
+++ source/repl.cpp
@@ -3,11 +3,58 @@
 
 #include <iostream>
 
-#include "tsbl/interpreter.hpp"
+#include "tsbl/utf8.hpp"
+
+using namespace tsbl::utf8;
+
+void output(codepoint_t pt) {
+    Category cat = category(pt);
+    std::cout << "Codepoint " << pt << ":" << std::endl
+        << "  Category: " << category_name(cat) << " [" << category_id(cat) << "|" << cat << "]" << std::endl;
+}
 
 int main(int argc, char **argv) {
-	tsbl::Interpreter interp;
-	int32_t codepoint = 50;
-	std::cout << "Codepoint " << codepoint << "'s category is " << interp.category(codepoint) << std::endl;
-	return -1;
+    for (int i = 1; i < argc; ++i) {
+        int codepoint = atoi(argv[i]);
+        if (codepoint > 0) {
+            output(codepoint);
+        }
+        else {
+            char * arg = argv[i];
+            while (*arg != '\0') {
+                if (*arg == '\\') {
+                    arg += 1;
+                    switch (*arg) {
+                    case 'a':
+                        codepoint = '\a';
+                        break;
+                    case 'b':
+                        codepoint = '\b';
+                        break;
+                    case 'f':
+                        codepoint = '\f';
+                        break;
+                    case 'n':
+                        codepoint = '\n';
+                        break;
+                    case 'r':
+                        codepoint = '\r';
+                        break;
+                    case 't':
+                        codepoint = '\t';
+                        break;
+                    case 'v':
+                        codepoint = '\v';
+                        break;
+                    }
+                }
+                else {
+                    codepoint = *arg;
+                }
+                arg += 1;
+                output(codepoint);
+            }
+        }
+    }
+	return 0;
 }
